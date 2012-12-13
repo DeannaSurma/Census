ee3a387e850c81d1153437056979b95c55fd80fc
Deanna Surma <dms424@cornell.edu>
2012-12-13 11:28:58 -0500

    xml changes

--- src/com/cs4302/census/client/Census.java
+++ src/com/cs4302/census/client/Census.java
@@ -4,8 +4,10 @@ import com.cs4302.census.client.service.InputService;
 import com.cs4302.census.client.service.InputServiceAsync;
 import com.cs4302.census.client.service.OutputService;
 import com.cs4302.census.client.service.OutputServiceAsync;
+import com.cs4302.census.shared.profiles.StateProfile;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.TextBox;
 
@@ -31,7 +33,7 @@ public class Census implements EntryPoint {
    * This is the entry point method.
    */
   public void onModuleLoad() {
-	  boolean state = true;
+	  boolean state = false;
 	  
 	  // state = true -> Mitch - displaying info
 	  // state = false -> Ken - uploading info
@@ -43,7 +45,35 @@ public class Census implements EntryPoint {
 		  
 	  }
 	  else{
-		  
+		  inputService.insertPlace(1l,"HI",2, 3,4,5,6, 1l,"HI",2, 3,4,5,6, 1l,"HI",2, 3,4,5,6, new AsyncCallback<Void>() {
+			   @Override  
+			   public void onFailure(Throwable caught) {
+		            System.out.println("FAILED");
+		        }
+
+				@Override
+				public void onSuccess(Void result) {
+					 System.out.println("SUCCESS");
+					// TODO Auto-generated method stub
+					outputService.getStateProfile(1l, new AsyncCallback<StateProfile>() {
+
+						@Override
+						public void onFailure(Throwable caught) {
+							// TODO Auto-generated method stub
+							
+						}
+
+						@Override
+						public void onSuccess(StateProfile result) {
+							System.out.println(result.toString());
+							// TODO Auto-generated method stub
+							
+						}
+						
+						
+					});
+				}
+		    });
 		  // read from file, iterate, use inputService
 		  // inputService.insertPlace(stateID, stateName, state70, state80, state90, state00, state10, countyID, countyName, county70, county80, county90, county00, county10, placeID, placeName, place70, place80, place90, place00, place10, new Async...)
 	  }
--- src/com/cs4302/census/server/service/impl/InputServiceImpl.java
+++ src/com/cs4302/census/server/service/impl/InputServiceImpl.java
@@ -18,39 +18,10 @@ public class InputServiceImpl extends RemoteServiceServlet implements InputServi
   public void insertPlace(long stateFP,  String stateName,  int state70,  int state80,  int state90,  int state00,  int state10,
 		  long countyFP, String countyName, int county70, int county80, int county90, int county00, int county10,
 		  long placeFP,  String placeName,  int place70,  int place80,  int place90,  int place00,  int place10){
+	  System.out.println("HERE");
 	  importProvider.addPlace(stateFP, stateName, state70, state80, state90, state00, state10, 
 			  countyFP, countyName, county70, county80, county90, county00, county10, 
 			  placeFP, placeName, place70, place80, place90, place00, place10);
   }
   
 }
-
-
-//public void addGoal(int userId, String title, String description) {
-//  
-//  Goal goal = new Goal(userId, title, description);
-//  ofy.put(goal);
-//  assert goal.getId() != null;
-//}
-//
-//public void removeGoal(Long goalId) {
-//  Key<Goal> goal = new Key<Goal>(Goal.class, goalId);
-//  ofy.delete(goal);
-//}
-//
-//public Goal getGoal(Long goalId) {
-//  Key<Goal> goal = new Key<Goal>(Goal.class, goalId);
-//  return ofy.get(goal);
-//}
-//
-//public Goal[] getUserGoals(Long[] userIds) {
-//  List<Goal> goals = new ArrayList<Goal>();
-//  for (Long userId : userIds) {
-//    Query<Goal> q = ofy.query(Goal.class).filter("userId =", userId);
-//    for (Goal goal: q) {
-//      goals.add(goal);
-//      System.out.println(goal.getTitle());
-//    }
-//  }
-//  return goals.toArray(new Goal[goals.size()]);
-//}
